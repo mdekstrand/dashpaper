45b89d345904e05c4b6003875825d0c16ce1aba8
Michael Ekstrand <md@ekstrandom.net>
2023-10-22 19:00:58 -0400

    try to display upcoming

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -2,9 +2,71 @@ import { useState } from "preact/hooks";
 import { Item } from "birch-outline";
 import "./HomeScreen.css";
 import { TaskPaperRepository } from "./storage/repo";
+import { TaskLine } from "./components/items";
 
 type HomeParams = {
   repo: TaskPaperRepository
+}
+
+function DocList({docs}) {
+  return (
+    <ul class="doclist">
+      {docs ? docs.map((o) => <li>
+        <a href={"/files/" + o.name}>{o.name}</a>
+      </li>) : []}
+    </ul>
+  );
+}
+
+function Upcoming({docs}) {
+  let seen = new Set();
+  let overdue = [];
+  let tomorrow = [];
+  let week = [];
+  let month = [];
+
+  for (let doc of docs) {
+    for (let item of doc.outline.evaluateItemPath("not @done and @due < [d] today")) {
+      overdue.push(item);
+      seen.add(item.id);
+    }
+    for (let item of doc.outline.evaluateItemPath("not @done and @due <= [d] tomorrow")) {
+      if (seen.has(item.id)) continue;
+      tomorrow.push(item);
+      seen.add(item.id);
+    }
+    for (let item of doc.outline.evaluateItemPath("not @done and @due <= [d] 8 days")) {
+      if (seen.has(item.id)) continue;
+      week.push(item);
+      seen.add(item.id);
+    }
+    for (let item of doc.outline.evaluateItemPath("not @done and @due <= [d] 1 month")) {
+      if (seen.has(item.id)) continue;
+      month.push(item);
+      seen.add(item.id);
+    }
+  }
+
+  return (
+    <div class="upcoming">
+      <div class="overdue">
+      <h2>Overdue</h2>
+      {overdue.map((i) => <TaskLine item={i} children={false} />)}
+      </div>
+      <div class="tomorrow">
+      <h2>Tomorrow</h2>
+      {tomorrow.map((i) => <TaskLine item={i} children={false} />)}
+      </div>
+      <div class="week">
+      <h2>This Week</h2>
+      {week.map((i) => <TaskLine item={i} children={false} />)}
+      </div>
+      <div class="month">
+      <h2>This Month</h2>
+      {month.map((i) => <TaskLine item={i} children={false} />)}
+      </div>
+    </div>
+  )
 }
 
 function HomeScreen(params: HomeParams) {
@@ -18,11 +80,8 @@ function HomeScreen(params: HomeParams) {
     <div class="container">
       <h1>DashPaper</h1>
 
-      <ul class="doclist">
-        {docs ? docs.map((o) => <li>
-          <a href={"/files/" + o.name}>{o.name}</a>
-        </li>) : []}
-      </ul>
+      <Upcoming docs={docs} />
+      <DocList docs={docs} />
     </div>
   );
 }
--- src/components/items.tsx
+++ src/components/items.tsx
@@ -2,6 +2,7 @@ import { Item } from "birch-outline";
 
 type TaskLineParams = {
   item: Item;
+  children?: boolean;
 }
 
 type TaskListParams = {
@@ -9,12 +10,13 @@ type TaskListParams = {
 }
 
 export function TaskLine(params: TaskLineParams) {
+  params.children ??= true;
   let item = params.item;
   console.log("item: %o", item);
   let type = item.getAttribute("data-type");
   return (<li class={"item " + type} id={item.id}>
     <div class="self">{item.bodyContentString}</div>
-    {item.children ? <TaskList items={item.children}/> : null}
+    {params.children && item.children ? <TaskList items={item.children}/> : null}
   </li>)
 }
 
